fce53c48dff20a8f23690aa643ab2034bc33752e
morihisashi <hisashi.mori@humanage.co.jp>
2023-03-14 12:29:11 +0900

    スプレッド演算子について

--- 060_spread_rest/start/main.js
+++ 060_spread_rest/start/main.js
@@ -1,25 +1,24 @@
 const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
+const result = Math.max(...nums);
 console.log(result);
 
 
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
-let newArr = [...arr1];
-
-console.log(newArr2);
+let newArr = [...arr1, 10, ...arr2];
+console.log(newArr);
 
 const obj = {
   name: "Tom",
   age: 22,
 };
 const newObj = { ...obj };
-
-console.log(newObj);
+newObj.name = 'John';
+console.log(newObj, obj);
 
 const restA = (...argA) => console.log(argA);
 restA(1, 3, 4)
 
-const restB = (n, ...argB) => console.log(argB);
+const restB = (n, ...argB) => console.log(n);
 restB(1, 3, 4)
